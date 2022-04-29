fb4d06fa4156f2078f9e4dde00e828d57c0f00c8
sharepointpro <michael@devpoint.com.au>
2022-04-29 23:31:23 +1000

    wip

--- src/webparts/imageMapperLandingPage/components/ImageMapperLandingPage.module.scss.ts
+++ src/webparts/imageMapperLandingPage/components/ImageMapperLandingPage.module.scss.ts
@@ -1,18 +1,18 @@
 /* tslint:disable */
 require("./ImageMapperLandingPage.module.css");
 const styles = {
-  imageMapperLandingPage: 'imageMapperLandingPage_32923665',
-  image: 'image_32923665',
-  pointer: 'pointer_32923665',
-  container: 'container_32923665',
-  row: 'row_32923665',
-  column: 'column_32923665',
-  'ms-Grid': 'ms-Grid_32923665',
-  title: 'title_32923665',
-  subTitle: 'subTitle_32923665',
-  description: 'description_32923665',
-  button: 'button_32923665',
-  label: 'label_32923665'
+  imageMapperLandingPage: 'imageMapperLandingPage_b8d605d7',
+  image: 'image_b8d605d7',
+  pointer: 'pointer_b8d605d7',
+  container: 'container_b8d605d7',
+  row: 'row_b8d605d7',
+  column: 'column_b8d605d7',
+  'ms-Grid': 'ms-Grid_b8d605d7',
+  title: 'title_b8d605d7',
+  subTitle: 'subTitle_b8d605d7',
+  description: 'description_b8d605d7',
+  button: 'button_b8d605d7',
+  label: 'label_b8d605d7'
 };
 
 export default styles;
--- src/webparts/imageMapperLandingPage/components/ImageMapperLandingPage.tsx
+++ src/webparts/imageMapperLandingPage/components/ImageMapperLandingPage.tsx
@@ -14,18 +14,22 @@ export default class ImageMapperLandingPage extends React.Component<IImageMapper
   }
 
   public render(): React.ReactElement<IImageMapperLandingPageProps> {
-    console.log("this.props", this.props);
+    
     return (
       <div className={styles.imageMapperLandingPage}>
 
-        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 1600 902">
+        <svg         
+        version="1.1" 
+        xmlns="http://www.w3.org/2000/svg" 
+        xmlnsXlink="http://www.w3.org/1999/xlink" 
+        viewBox="0 0 1600 902">
           <image width={this.props.imageWidth} height={this.props.imageHeight} xlinkHref={this.props.imageUrl}></image>
 
           {this.props.items.map((imageMapping, index) => {
             return (
               <a className={styles.pointer} onClick={() => this.onClick(imageMapping.url, imageMapping.openInNewWindow)}>
                 <rect x={imageMapping.x} y={imageMapping.y} fill="#fff" opacity="0" width={imageMapping.width} height={imageMapping.height}></rect>
-              </a>)
+              </a>);
           })}
 
         </svg>
