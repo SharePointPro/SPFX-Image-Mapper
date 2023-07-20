937276cc903ec193880966114fda552fb4152322
Alec Tecson <tecsonalr@gmail.com>
2023-07-21 07:51:54 +0800

    Removing node_modules, dist and temp files.

--- lib/webparts/imageMapperLandingPage/components/IImageMapperLandingPageProps.d.ts
+++
@@ -1,9 +0,0 @@
-import { IMapArea } from "../ImageMapperLandingPageWebPart";
-export interface IImageMapperLandingPageProps {
-    description: string;
-    imageUrl: string;
-    imageHeight: string;
-    imageWidth: string;
-    items: IMapArea[];
-}
-//# sourceMappingURL=IImageMapperLandingPageProps.d.ts.map
--- lib/webparts/imageMapperLandingPage/components/ImageMapperLandingPage.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import { IImageMapperLandingPageProps } from './IImageMapperLandingPageProps';
-export default class ImageMapperLandingPage extends React.Component<IImageMapperLandingPageProps, {}> {
-    onClick(link: string, openInNewWindow: boolean): void;
-    render(): React.ReactElement<IImageMapperLandingPageProps>;
-}
-//# sourceMappingURL=ImageMapperLandingPage.d.ts.map
